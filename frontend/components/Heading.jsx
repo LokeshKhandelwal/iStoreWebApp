202807741b72ca06e98d0403835042b213a358c6
Lokesh Khandelwal <lokeshkhandelwal2001@gmail.com>
2023-02-12 04:20:11 +0530

    Frontend Almost Complete

--- frontend/components/Heading.jsx
+++ frontend/components/Heading.jsx
@@ -1,11 +1,11 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-const Heading = ({text1="Our",text2="Products",containerStyle}) => {
+const Heading = ({text1="iStore",text2="",containerStyle}) => {
     return (
         <View style={containerStyle}>
-            <Text style={{ fontSize: 25 }}>{text1}</Text>
-            <Text style={{ fontSize: 25, fontWeight: "900" }}>{text2}</Text>
+            <Text style={{ fontSize: 25,fontWeight: "900" ,marginTop:-50}}>{text1}</Text>
+            <Text style={{ fontSize: 25,  }}>{text2}</Text>
         </View>
     )
 }
